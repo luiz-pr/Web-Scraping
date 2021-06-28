392957aa528ee33ce0a32849a6371308402fc1a9
luiz-pr <76491544+luiz-pr@users.noreply.github.com>
2021-06-28 13:43:38 -0300

    Primeiro commit

---
+++ webscraping.py
@@ -0,0 +1,56 @@
+import json
+import time
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+url = "https://www.nba.com/stats/players/traditional/?sort=PTS&dir=-1"
+top10ranking = {}
+
+# rankings = {
+#     '3points': {'field': 'FG3M,', 'label': '3PM'},
+#     '3points': {'field': 'PTS,', 'label': '3PM'},
+#     '3points': {'field': 'AST,', 'label': '3PM'},
+#     '3points': {'field': 'REB,', 'label': '3PM'},
+#     '3points': {'field': 'STL,', 'label': '3PM'},
+#     '3points': {'field': 'BLK,', 'label': '3PM'},
+# }
+
+option = Options()
+option.headless = True
+driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
+
+
+driver.get(url)
+time.sleep(5)
+
+
+driver.find_element_by_xpath('//div[@class="nba-stat-table"]//table//thead//tr//th[@data-field="PTS"]').click()
+
+
+element = driver.find_element_by_xpath('//div[@class="nba-stat-table"]//table')
+html_content = element.get_attribute('outerHTML')
+
+
+soup = BeautifulSoup(html_content, 'html.parser')
+table = soup.find(name='table')
+
+
+df_full = pd.read_html(str(table))[0].head(10)
+df = df_full[['Unnamed: 0', 'PLAYER', 'TEAM', 'PTS']]
+df.columns = ['pos', 'player', 'team', 'total']
+
+
+top10ranking['points'] =df.to_dict('records')
+
+
+driver.quit()
+
+
+js = json.dumps(top10ranking)
+fp = open('ranking.json', 'w')
+fp.write(js)
+fp.close()
